10dc42de92cd1d9978a628ccf817220c9ed2f7d3
AwaabSingh <adeoyin2828@gmail.com>
2023-01-20 19:50:33 +0100

    Write up

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,53 +1,53 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api/users/';
-// const API_URL = 'https://encrypt-be.onrender.com/api/users/';
+// const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = "https://encrypt-be.onrender.com/api/users/";
 
 // Register user
 const register = async (userData) => {
-	const response = await axios.post(API_URL + 'register', userData);
+  const response = await axios.post(API_URL + "register", userData);
 
-	if (response.data.token !== null) {
-		sessionStorage.setItem('user', JSON.stringify(response.data));
-	}
+  if (response.data.token !== null) {
+    sessionStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-	return response.data;
+  return response.data;
 };
 
 // Login user
 const login = async (userData) => {
-	const response = await axios.post(API_URL + 'login', userData);
+  const response = await axios.post(API_URL + "login", userData);
 
-	if (response.data.token !== null) {
-		sessionStorage.setItem('user', JSON.stringify(response.data));
-	}
+  if (response.data.token !== null) {
+    sessionStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-	return response.data;
+  return response.data;
 };
 
 // get me
 const getMe = async (token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL + 'me', config);
+  const response = await axios.get(API_URL + "me", config);
 
-	return response.data;
+  return response.data;
 };
 
 // Logout user
 const logout = () => {
-	sessionStorage.removeItem('user');
+  sessionStorage.removeItem("user");
 };
 
 const authService = {
-	register,
-	logout,
-	login,
-	getMe,
+  register,
+  logout,
+  login,
+  getMe,
 };
 
 export default authService;
--- src/features/vault/vaultService.js
+++ src/features/vault/vaultService.js
@@ -1,75 +1,75 @@
-import axios from 'axios';
+import axios from "axios";
 
-// export const API_URL = 'https://encrypt-be.onrender.com/api/vaults/';
-const API_URL = 'http://localhost:5000/api/vaults/';
+export const API_URL = "https://encrypt-be.onrender.com/api/vaults/";
+// const API_URL = 'http://localhost:5000/api/vaults/';
 
 // Create nen vault item
 const addVault = async (vaultData, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(
-		API_URL + 'add-password',
-		vaultData,
-		config
-	);
+  const response = await axios.post(
+    API_URL + "add-password",
+    vaultData,
+    config
+  );
 
-	// console.log(response);
-	return response.data;
+  // console.log(response);
+  return response.data;
 };
 
 // Get user goals
 const getVaults = async (token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.get(API_URL, config);
+  const response = await axios.get(API_URL, config);
 
-	return response.data;
+  return response.data;
 };
 
 // Delete user vault
 const deleteVault = async (vaultId, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.delete(API_URL + vaultId, config);
+  const response = await axios.delete(API_URL + vaultId, config);
 
-	// console.log(response.data);
-	return response.data;
+  // console.log(response.data);
+  return response.data;
 };
 
 // decrpyt password
 const decrpytPassword = async (vaultData, token) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	};
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-	const response = await axios.post(
-		API_URL + 'decrypt-password',
-		vaultData,
-		config
-	);
+  const response = await axios.post(
+    API_URL + "decrypt-password",
+    vaultData,
+    config
+  );
 
-	return response.data;
+  return response.data;
 };
 
 const vaultService = {
-	addVault,
-	getVaults,
-	deleteVault,
-	decrpytPassword,
+  addVault,
+  getVaults,
+  deleteVault,
+  decrpytPassword,
 };
 
 export default vaultService;
